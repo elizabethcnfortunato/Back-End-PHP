f6025f6af2a2bb21a8948130c9a691172f52eb3a
Elizabeth Fortunato <elizabethcnfortunato@gmail.com>
2018-11-12 17:36:24 -0200

    primeiro commit do estudo INICIANTE BACK-END PHP

---
+++ adiciona-produto.php
@@ -0,0 +1,9 @@
+<html>
+	<?php 
+	$nome = $_GET["nome"];
+	$preco = $_GET["preco"];
+	?>
+	Produto: <?= $nome; ?> <br>
+	Valor de: <?= $preco?> <br><br>
+	Adicionado com sucesso!
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<html>
+	<h1>Bem Vindo!</h1>
+</html>
